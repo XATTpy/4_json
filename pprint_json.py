991aef17133932fb307418a3edf61e99310c8af6
XATTpy <zoomerrr@mail.ru>
2019-06-20 02:45:37 +0300

    fix exceptions

--- pprint_json.py
+++ pprint_json.py
@@ -2,25 +2,28 @@ import json
 import sys
 from sys import argv
 
-# Functions
+
 def load_data(filepath):
     with open(filepath, "r") as json_file:
         return json.loads(json_file.read())
 
 
-def pretty_print_json(ugly_json):
-    return json.dumps(ugly_json, sort_keys=True, indent=4, ensure_ascii=False)
-
-
-# Exeptions and variable initialization
-try:
-    filepath = argv[1]
-    ugly_json = load_data(filepath)
-except (IndexError, IsADirectoryError, FileNotFoundError):
-    sys.exit("Введите путь к файлу в качестве аргумента при запуске. Прим.: python3 bars.py /path_to_file/file_name.json")
-except ValueError:
-    sys.exit("Файл имеет неверный формат.")
+def pretty_print_json(decoded_json):
+    return json.dumps(decoded_json, sort_keys=True, indent=4, ensure_ascii=False)
 
 
 if __name__ == "__main__":
-    print(pretty_print_json(ugly_json))
+    try:
+        filepath = argv[1]
+        decoded_json = load_data(filepath)
+    except IndexError:
+        sys.exit("Введите путь к файлу в качестве аргумента при запуске.")
+    except IsADirectoryError:
+        sys.exit("Вы ввели путь к файлу, не указав самого файла.")
+    except FileNotFoundError:
+        sys.exit("Такого файла не существует")
+    except ValueError:
+        sys.exit("Файл имеет неверный формат.")
+
+
+    print(pretty_print_json(decoded_json))
