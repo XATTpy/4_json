f79338af1cb625ce9464d9c4f0ae2d0bb183a1cb
XATTpy <zoomerrr@mail.ru>
2019-06-20 13:02:30 +0300

    fix exceptions

--- pprint_json.py
+++ pprint_json.py
@@ -4,8 +4,13 @@ from sys import argv
 
 
 def load_data(filepath):
-    with open(filepath, "r") as json_file:
-        return json.loads(json_file.read())
+    try:
+        with open(filepath, "r") as json_file:
+            return json.loads(json_file.read())
+    except FileNotFoundError:
+        sys.exit("Такого файла не существует")
+    except ValueError:
+        sys.exit("Файл имеет неверный формат.")
 
 
 def pretty_print_json(decoded_json):
@@ -16,14 +21,8 @@ if __name__ == "__main__":
     try:
         filepath = argv[1]
         decoded_json = load_data(filepath)
-    except IndexError:
+    except (IndexError, IsADirectoryError):
         sys.exit("Введите путь к файлу в качестве аргумента при запуске.")
-    except IsADirectoryError:
-        sys.exit("Вы ввели путь к файлу, не указав самого файла.")
-    except FileNotFoundError:
-        sys.exit("Такого файла не существует")
-    except ValueError:
-        sys.exit("Файл имеет неверный формат.")
 
 
     print(pretty_print_json(decoded_json))
