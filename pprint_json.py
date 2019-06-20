23bce2ba2a7986b72a893ebc4109fed0dc5b46f8
XATTpy <zoomerrr@mail.ru>
2019-06-20 14:35:01 +0300

    add func main()

--- pprint_json.py
+++ pprint_json.py
@@ -4,25 +4,27 @@ from sys import argv
 
 
 def load_data(filepath):
-    try:
-        with open(filepath, "r") as json_file:
-            return json.loads(json_file.read())
-    except FileNotFoundError:
-        sys.exit("Такого файла не существует")
-    except ValueError:
-        sys.exit("Файл имеет неверный формат.")
+    with open(filepath, "r") as json_file:
+        return json.loads(json_file.read())
 
 
 def pretty_print_json(decoded_json):
     return json.dumps(decoded_json, sort_keys=True, indent=4, ensure_ascii=False)
 
 
-if __name__ == "__main__":
+def main():
     try:
         filepath = argv[1]
         decoded_json = load_data(filepath)
+        print(pretty_print_json(decoded_json))
     except (IndexError, IsADirectoryError):
         sys.exit("Введите путь к файлу в качестве аргумента при запуске.")
+    except FileNotFoundError:
+        sys.exit("Такого файла не существует")
+    except ValueError:
+        sys.exit("Файл имеет неверный формат.")
 
 
-    print(pretty_print_json(decoded_json))
+
+if __name__ == "__main__":
+    main()
